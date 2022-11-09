95012bef68b89da6ea51c5ea0f9e2d097d092708
laoxian96 <550475663@qq.com>
2022-11-09 15:32:21 +0800

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, Jenkins!!!！!!");
+    res.send("Hello, Jenkins!!!!!");
 });
 
 app.listen(8888, () => {
